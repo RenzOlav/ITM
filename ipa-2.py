bb9156f27e9e167687115ad83a88a1763f25add6
RenzOlav <renzolavasuncion@gmail.com>
2023-07-07 08:43:33 +0800

    Resubmit lang ng complete ipa-2.py

--- ipa-2.py
+++ ipa-2.py
@@ -1,10 +1,3 @@
-'''Individual Programming Assignment 2
-
-70 points
-
-This assignment will develop your proficiency with Python's control flows.
-'''
-
 def shift_letter(letter, shift):
     if letter == " ":
         return " "
@@ -19,14 +12,6 @@ def shift_letter(letter, shift):
         if letter.isupper():
             shifted_letter = shifted_letter.upper()
         return shifted_letter
-
-
-
-
-# Example usage
-input_letter = str(input("Enter a letter: "))
-shift_amount = int(input("Enter the shift amount: "))
-print(shift_letter(input_letter,shift_amount))
 
 def caesar_cipher(message, shift):
     shifted_message = ""  # Variable to store the shifted message
@@ -46,11 +31,6 @@ def caesar_cipher(message, shift):
             shifted_message += shifted_letter
     return shifted_message
 
-
-messager=str(input("Put message:"))
-shifty=int(input("Number of shift:"))
-print(caesar_cipher(messager, shifty))
-
 def shift_by_letter(letter, letter_shift):
     if letter == " ":
         return " "
@@ -68,12 +48,6 @@ def shift_by_letter(letter, letter_shift):
             shifted_letter = shifted_letter.upper()
 
         return shifted_letter
-
-liter = str(input("input a letter buddy: "))
-liter_ship = str(input("isa pang letter buddy: "))
-result = shift_by_letter(liter, liter_ship)
-print(result)
-
 
 def vigenere_cipher(message, key):
     ciphertext = ""
@@ -99,12 +73,6 @@ def vigenere_cipher(message, key):
             ciphertext += letter
     return ciphertext
 
-
-message = input("Ano message mo?: ")
-key = input("bigay ka isang word: ")
-print("Encrypted text:", vigenere_cipher(message, key))
-
-
 def scytale_cipher(message, shift):
     message_length = len(message)
 
@@ -119,13 +87,6 @@ def scytale_cipher(message, shift):
         encoded_message += message[index]
 
     return encoded_message
-
-
-message = input("Enter the message: ")
-shift = int(input("Enter the shift: "))
-
-encrypted_message = scytale_cipher(message, shift)
-print("Encrypted message:", encrypted_message)
 
 def scytale_decipher(message, shift):
     # Determine the length of each rail
@@ -146,10 +107,3 @@ def scytale_decipher(message, shift):
 
     return deciphered_message
 
-
-
-text_encrypt = input("Enter the ciphertext: ")
-shift = int(input("Enter the diameter of the Scytale: "))
-
-decrypted_text = scytale_decipher(text_encrypt, shift)
-print("Decrypted text:", decrypted_text)
