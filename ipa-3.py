430801024a5a5cc55397fb0c4c6ce5d2cd720f45
RenzOlav <renzolavasuncion@gmail.com>
2023-07-08 19:11:19 +0800

    Resubmit lang ng complete ipa-2.py

--- ipa-3.py
+++ ipa-3.py
@@ -1,182 +1,49 @@
-'''
-Sample data for Relationship Status below:
-'''
 
-social_graph = {
-    "@bongolpoc":{"first_name":"Joselito",
-                  "last_name":"Olpoc",
-                  "following":[
-                  ]
-    },
-    "@joaquin":  {"first_name":"Joaquin",
-                  "last_name":"Gonzales",
-                  "following":[
-                      "@chums","@jobenilagan"
-                  ]
-    },
-    "@chums" : {"first_name":"Matthew",
-                "last_name":"Uy",
-                "following":[
-                    "@bongolpoc","@miketan","@rudyang","@joeilagan"
-                ]
-    },
-    "@jobenilagan":{"first_name":"Joben",
-                   "last_name":"Ilagan",
-                   "following":[
-                    "@eeebeee","@joeilagan","@chums","@joaquin"
-                   ]
-    },
-    "@joeilagan":{"first_name":"Joe",
-                  "last_name":"Ilagan",
-                  "following":[
-                    "@eeebeee","@jobenilagan","@chums"
-                  ]
-    },
-    "@eeebeee":  {"first_name":"Elizabeth",
-                  "last_name":"Ilagan",
-                  "following":[
-                    "@jobenilagan","@joeilagan"
-                  ]
-    },
-}
-
-'''
-Sample data for Tic Tac Toe below:
-'''
-
-board1 = [
-['X','X','O'],
-['O','X','O'],
-['O','','X'],
-]
-
-board2 = [
-['X','X','O'],
-['O','X','O'],
-['','O','X'],
-]
-
-board3 = [
-['O','X','O'],
-['','O','X'],
-['X','X','O'],
-]
-
-board4 = [
-['X','X','X'],
-['O','X','O'],
-['O','','O'],
-]
-
-board5 = [
-['X','X','O'],
-['O','X','O'],
-['X','','O'],
-]
-
-board6 = [
-['X','X','O'],
-['O','X','O'],
-['X','',''],
-]
-
-board7 = [
-['X','X','O',''],
-['O','X','O','O'],
-['X','','','O'],
-['O','X','','']
-]
-
-'''
-Sample data for ETA below:
-
-(from_stop, to_stop)
-'''
-
-legs = {
-     ("upd","admu"):{
-         "travel_time_mins":10
-     },
-     ("admu","dlsu"):{
-         "travel_time_mins":35
-     },
-     ("dlsu","upd"):{
-         "travel_time_mins":55
-     }
-}
-
-legs = {
-    ('a1', 'a2'): {
-        'travel_time_mins': 10
-    },
-    ('a2', 'b1'): {
-        'travel_time_mins': 10230
-    },
-    ('b1', 'a1'): {
-        'travel_time_mins': 1
-    }
-}
 def relationship_status(from_member, to_member, social_graph):
-
+    if to_member in social_graph.get(from_member, {}).get("following", []):
+        if from_member in social_graph.get(to_member, {}).get("following", []):
+            return "friends"
+        else:
+            return "follower"
+    elif from_member in social_graph.get(to_member, {}).get("following", []):
+        return "followed by"
+    else:
+        return "no relationship"
 
 
 def tic_tac_toe(board):
-    '''Tic Tac Toe.
-    25 points.
+    # Check rows
+    for row in board:
+        if all(cell == row[0] and cell for cell in row):
+            return row[0]
 
-    Tic Tac Toe is a common paper-and-pencil game.
-    Players must attempt to successfully draw a straight line of their symbol across a grid.
-    The player that does this first is considered the winner.
+    # Check columns
+    for col in range(len(board)):
+        if all(board[row][col] == board[0][col] and board[row][col] for row in range(len(board))):
+            return board[0][col]
 
-    This function evaluates a tic tac toe board and returns the winner.
+    # Check diagonals
+    if all(board[i][i] == board[0][0] and board[i][i] for i in range(len(board))):
+        return board[0][0]
 
-    Please see "assignment-4-sample-data.py" for sample data. The board will adhere
-    to the same pattern. The board may by 3x3, 4x4, 5x5, or 6x6. The board will never
-    have more than one winner. The board will only ever have 2 unique symbols at the same time.
+    if all(board[i][len(board) - 1 - i] == board[0][len(board) - 1] and board[i][len(board) - 1 - i] for i in range(len(board))):
+        return board[0][len(board) - 1]
 
-    Parameters
-    ----------
-    board: list
-        the representation of the tic-tac-toe board as a square list of lists
+    return "NO WINNER"
 
-    Returns
-    -------
-    str
-        the symbol of the winner or "NO WINNER" if there is no winner
-    '''
-    # Replace `pass` with your code.
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
 
 def eta(first_stop, second_stop, route_map):
-    '''ETA.
-    25 points.
+    # Check if the first_stop and second_stop are in the route_map
+    if (first_stop, second_stop) in route_map:
+        return route_map[(first_stop, second_stop)]['travel_time_mins']
 
-    A shuttle van service is tasked to travel along a predefined circlar route.
-    This route is divided into several legs between stops.
-    The route is one-way only, and it is fully connected to itself.
+    # Iterate over each leg in the route_map
+    for leg in route_map:
+        # Check if the first_stop matches the starting point of the leg
+        if leg[0] == first_stop:
+            # Check if the second_stop matches the ending point of the leg
+            if leg[1] == second_stop:
+                return route_map[leg]['travel_time_mins']
 
-    This function returns how long it will take the shuttle to arrive at a stop
-    after leaving another stop.
-
-    Please see "mod-4-ipa-1-sample-data.py" for sample data. The route map will
-    adhere to the same pattern. The route map may contain more legs and more stops,
-    but it will always be one-way and fully enclosed.
-
-    Parameters
-    ----------
-    first_stop: str
-        the stop that the shuttle will leave
-    second_stop: str
-        the stop that the shuttle will arrive at
-    route_map: dict
-        the data describing the routes
-
-    Returns
-    -------
-    int
-        the time it will take the shuttle to travel from first_stop to second_stop
-    '''
-    # Replace `pass` with your code.
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    # If no direct leg is found, return -1 to indicate an invalid route
+    return "INVALID"
