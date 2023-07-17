6090f22128d0fb0d9ff31956a4f46eefd804bc84
RenzOlav <renzolavasuncion@gmail.com>
2023-07-17 21:13:48 +0800

    Resubmit ng ipa-3.py HEHE

--- ipa-3.py
+++ ipa-3.py
@@ -31,14 +31,62 @@ def tic_tac_toe(board):
 
     return "NO WINNER"
 
+legs = {
+    ('a0', 'a1'): {
+        'travel_time_mins': 2
+    },
+    ('a1', 'a2'): {
+        'travel_time_mins': 10
+    },
+    ('a2', 'a3'): {
+        'travel_time_mins': 34
+    },
+    ('a3', 'a4'): {
+        'travel_time_mins': 145123
+    },
+    ('a4', 'a5'): {
+        'travel_time_mins': 1239
+    },
+    ('a5', 'a6'): {
+        'travel_time_mins': 1935
+    },
+    ('a6', 'a7'): {
+        'travel_time_mins': 1891345
+    },
+    ('a7', 'a8'): {
+        'travel_time_mins': 789253
+    },
+    ('a8', 'a9'): {
+        'travel_time_mins': 78234
+    },
+    ('a9', 'a10'): {
+        'travel_time_mins': 432678
+    },
+    ('a10', 'a0'): {
+        'travel_time_mins': 1
+    }
+}
 
 def eta(first_stop, second_stop, route_map):
-    if (first_stop, second_stop) in route_map:
-        return route_map[(first_stop, second_stop)]['travel_time_mins']
+    current_stop = first_stop
+    total_time = 0
 
-    for leg in route_map:
-        if leg[0] == first_stop:
-            if leg[1] == second_stop:
-                return route_map[leg]['travel_time_mins']
+    while current_stop != second_stop:
+        next_stop = None
+        travel_time = None
 
-    return "INVALID"
+        for leg, details in route_map.items():
+            if leg[0] == current_stop:
+                next_stop = leg[1]
+                travel_time = details['travel_time_mins']
+                break
+
+        if next_stop is None:
+            raise ValueError("No valid route found between the stops.")
+
+        total_time += travel_time
+
+        current_stop = next_stop
+    return total_time
+
+
